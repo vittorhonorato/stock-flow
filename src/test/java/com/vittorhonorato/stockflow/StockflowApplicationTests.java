5e218e891ded0cd7a53d0f6eabc82de41cf5e861
Vitor Honorato <vittorhonoratoo@gmail.com>
2026-04-29 21:58:57 -0300

    Add Spring Boot project files

---
+++ src/test/java/com/vittorhonorato/stockflow/StockflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.stockflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
